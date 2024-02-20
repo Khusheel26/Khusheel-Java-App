0556593e5a8b6ff04df9d705723cd7567b6c48b7
Khusheel26 <khusheel26@gmail.com>
2024-02-20 10:16:48 +0530

    Added the default package files

---
+++ src/test/java/com/octobit8/KhusheelApp/KhusheelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octobit8.KhusheelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhusheelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
